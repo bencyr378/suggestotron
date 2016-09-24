a00f1b3eeecb5e1f84f3aa3aa0e6a345d0c8d886
Benjamin Cyr <bencyr378@gmail.com>
2016-09-24 15:31:49 -0400

    Edited buttons and links

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -28,7 +28,7 @@ class TopicsController < ApplicationController
 
     respond_to do |format|
       if @topic.save
-        format.html { redirect_to @topic, notice: 'Topic was successfully created.' }
+        format.html { redirect_to topics_path, notice: 'Topic was successfully created.' }
         format.json { render :show, status: :created, location: @topic }
       else
         format.html { render :new }
@@ -60,6 +60,18 @@ class TopicsController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  def upvote
+    @topic = Topic.find(params[:id])
+    @topic.votes.create
+    redirect_to(topics_path)
+  end
+
+  def downvote
+    @topic = Topic.find(params[:id])
+    @topic.votes.destroy
+    redirect_to(topics_path)
+  end  
 
   private
     # Use callbacks to share common setup or constraints between actions.
